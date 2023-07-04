b07e3327cfc928778a19fce9f8fce8de0a8918d7
Mohsin-440 <mohsinali033400@gmail.com>
2023-07-05 01:13:40 +0500

    first commit

---
+++ src/Components/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p className="footer-text">Created by: Mohsin Ali</p>
+        <p className="footer-text">Date: 22 June 2023</p>
+        <p className="footer-text">Contact: +92 3310332215</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
